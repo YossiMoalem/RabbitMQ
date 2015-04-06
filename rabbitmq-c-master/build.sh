bd1541cd0bb390c7424d0e47d567ad493ee3fd1d
Yossi Moalem <moalem.yossi@gmail.com>
2015-04-06 15:50:03 +0300

    chore(RabbitMQ): removed old clients
    Change-Id: Iffbcdfd3db261db9f0c5ef09e8613571f3b6fce9

--- rabbitmq-c-master/build.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build
-mkdir build && cd build
-cmake ../
-cmake --build .
-cd librabbitmq
-sudo make install
-pwd
-ln -sf ./build/librabbitmq/librabbitmq.so ../../
-ln -sf ./build/librabbitmq/librabbitmq.so.1 ../../
