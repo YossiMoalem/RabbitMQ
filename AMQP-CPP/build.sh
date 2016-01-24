59257849327e0eb0c74c591e3fea1c716ce5f1aa
Yossi Moalem <moalem.yossi@gmail.com>
2016-01-24 12:05:40 +0200

    feature: run new local hubs in whiteboxes
    Change-Id: I5113dee615f6b27674fd43cdfad7e3ebc998df64

--- AMQP-CPP/build.sh
+++ AMQP-CPP/build.sh
@@ -1,4 +1,5 @@
 make clean
 make
-#TODO: after layer2 moves from example/2, we wont need the "ln -s" line
+#TODO: after layer2 moves from example/2, we wont need the "ln -s" lines
 ln -fs src/libamqpcpp.so .
+ln -fs src/libamqpcpp.a .
